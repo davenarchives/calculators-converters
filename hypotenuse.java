22cb3bb2396e964f0cd505587464b3a12b8a49ba
Daven's Archives <160004612+davenarchives@users.noreply.github.com>
2024-09-15 21:47:02 +0800

    hypotenuse calculator

---
+++ hypotenuse.java
@@ -0,0 +1,26 @@
+/*
+ * The hypotenuse is the longest side of a right-angled triangle, 
+ *located opposite the right angle. It is the side that connects the two legs 
+ *(the other two sides) of the triangle and is fundamental in the Pythagorean theorem, 
+ *which states that the square of the hypotenuse is equal to the sum of the squares of the other two sides. 
+ */
+
+import java.util.Scanner;
+
+public class hypotenuse {
+    public static void main(String[] args){
+        double a;
+        double b;
+        double c;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter side a: ");
+        a = scanner.nextDouble();
+
+        System.out.print("Enter side b: ");
+        b = scanner.nextDouble();
+
+        c = Math.sqrt((a*a)+(b*b));
+        System.out.println("The hypotenuse is: "+c);
+    }
+}
