8d2773a788ffbab522c5145b508de2351fc14c4f
davenarchives <daventheabyss@gmail.com>
2024-09-16 13:53:17 +0800

    circumference calculator

--- circumference.java
+++ circumference.java
@@ -14,9 +14,9 @@ public class circumference {
             System.out.print("Enter the radius: ");
             double radius = scanner.nextDouble();
 
-            double pi = 3.141592653589793238462643383279502884197;
+            final double pi = 3.14159;
     
-            double circumference = 2*pi*radius;
+            final double circumference = 2*pi*radius;
             System.out.println("The circumference of the circle is: "+circumference);
         }
       
